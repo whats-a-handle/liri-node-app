76ba75965243238c3ebad3c08fad86cbfb413723
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-29 00:07:43 -0800

    Transferred twitter-related code to twitterhandler.js

--- liri.js
+++ liri.js
@@ -1,42 +1,5 @@
 require("dotenv").config();
 
-function getTweets(){
-
-	const createTwitter = require('twitter');
-		const twitterCredentials = require("./keys").twitter;
-		const Twitter = new createTwitter({
-			consumer_key: twitterCredentials.consumer_key,
-		  	consumer_secret: twitterCredentials.consumer_secret,
-		  	access_token_key: twitterCredentials.access_token_key,
-		  	access_token_secret: twitterCredentials.access_token_secret
-		});
-
-		const params = {screen_name: input.query};
-		Twitter.get('statuses/user_timeline', function(error, tweets, response) {
-			if (!error) {
-				let parsedTweets = [];
-				tweets.map(function(tweet){
-
-					parsedTweets.push(parseTweet(tweet));
-
-				});
-				console.log(parsedTweets);
-			}});
-
-}
-
-function parseTweet(data){
-
-	const Tweet = {
-
-		author : data.user.name,
-		timeStamp : data.user.created_at,
-		text : data.text
-	}
-
-	return Tweet;
-}
-
 
 
 function getInput(){
@@ -54,8 +17,15 @@ function processInput(input){
 
 	if(input.command === 'my-tweets'){
 		
-		getTweets();
+		const TwitterHandler = require('./twitterhandler.js');
+		
+		TwitterHandler.getTweets(input.query);
 
+	}
+	else if(input.command ==='new-tweet'){
+		const TwitterHandler = require('./twitterhandler.js');
+		
+		TwitterHandler.postTweet(input.query);
 
 	}
 	else if(input.command === 'spotify-this-song'){
