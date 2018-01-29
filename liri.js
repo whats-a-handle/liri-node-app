951648d0c824d55c4564104bca8ef58a50c314b8
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-28 21:50:15 -0800

    Completed Twitter module

--- liri.js
+++ liri.js
@@ -1,8 +1,86 @@
 require("dotenv").config();
 
-const twitter = require("./keys").twitter;
-const spotify = require("./keys").spotify;
+function getTweets(){
 
-console.log(twitter);
-console.log(spotify);
+	const createTwitter = require('twitter');
+		const twitterCredentials = require("./keys").twitter;
+		const Twitter = new createTwitter({
+			consumer_key: twitterCredentials.consumer_key,
+		  	consumer_secret: twitterCredentials.consumer_secret,
+		  	access_token_key: twitterCredentials.access_token_key,
+		  	access_token_secret: twitterCredentials.access_token_secret
+		});
+
+		const params = {screen_name: input.query};
+		Twitter.get('statuses/user_timeline', function(error, tweets, response) {
+			if (!error) {
+				let parsedTweets = [];
+				tweets.map(function(tweet){
+
+					parsedTweets.push(parseTweet(tweet));
+
+				});
+				console.log(parsedTweets);
+			}});
+
+}
+
+function parseTweet(data){
+
+	const Tweet = {
+
+		author : data.user.name,
+		timeStamp : data.user.created_at,
+		text : data.text
+	}
+
+	return Tweet;
+}
+
+
+
+function getInput(){
+
+	input = {
+		command : process.argv[2],
+		query : process.argv[3],
+	}
+
+	return input;
+}
+
+function processInput(input){
+
+
+	if(input.command === 'my-tweets'){
+		
+		getTweets();
+
+
+	}
+	else if(input.command === 'spotify-this-song'){
+
+		const spotifyCredentials = require("./keys").spotify;
+		console.log('Searching spotify for ' + input.query);
+		
+	}
+	else if(input.command === 'movie-this'){
+
+		console.log('Searching for the movie ' + input.query);
+
+	}
+
+
+}
+
+
+
+
+processInput(getInput());
+
+
+
+
+
+
 
