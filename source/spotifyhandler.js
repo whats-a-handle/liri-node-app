8e31eb036003a08faa36cba65546d51e11ab5d73
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-30 19:17:05 -0800

    Added function to parseSongs into better format Need to move parseSong function outside of createSong function

--- source/spotifyhandler.js
+++ source/spotifyhandler.js
@@ -18,22 +18,43 @@ function createSpotifyHandler(){
 			return Spotify;
 
 		},
+		createSong : function(data){
+			const Song = {
+				artist : '',
+				name : data.name,
+				album : data.album.name,
+				preview_url : data.preview_url,
+				parseArtists : function(){
+					const Song = this;
 
+					data.artists.map(function(artist){Song.artist += artist.name});
+
+				}
+			};
+
+			Song.parseArtists();
+
+			return Song;
+
+		},
 		findSong : function(input){
+			const SpotifyHandler = this;
 			const params = {
 				type : 'track',
 				query : input,
-				limit : 1,
+				limit : 5,
 			};
 
-			this.Spotify.search(params, function(error, data){
+			SpotifyHandler.Spotify.search(params, function(error, data){
 
 				if(error){
 					return console.log('Error occurred: ' + error);
 				}
 				else{
-
-					console.log(data.tracks);
+					const Songs = [];
+					data.tracks.items.map(function(song){Songs.push(SpotifyHandler.createSong(song))});
+					console.log(Songs);
+				
 				}
 			});
 
