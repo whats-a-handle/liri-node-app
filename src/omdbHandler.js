6daea2c05ac93c19c6910b373dc329e7a90907bb
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-02-03 11:06:56 -0800

    Functioning OMDB module for movie lookup

--- src/omdbHandler.js
+++ src/omdbHandler.js
@@ -1,20 +1,68 @@
-function createOMDBHandler(){
 
-	const OMDBHander = {
+   const OMDBHandler = {
+
+      OMDB : {},
+      
+      findMovie : function(movieName){
+         const OMDBHandler = this;
+         const Request = require('request');
+         const searchParam = "&t=" + movieName;
+         Request(this.OMDB.queryURL + searchParam,function(error, response,body){
+
+            if(error){
+               console.log("Error occurred during search of movie: " + movieName);
+               console.log(error);
+               console.log("Response Code: " + response);
+            }
+            else{
+               
+               console.log(OMDBHandler.parseMovie(body));
+
+            }
+            
+         });
+      },
+
+      createMovie : function(movieData){
+
+         const Movie = {
+            title : movieData.Title,
+            year : movieData.Year,
+            type : movieData.Type,
+            plot : movieData.Plot,
+            imdbRating : movieData.imdbRating,
+            tomatoRating : movieData.tomatoRating,
+            country : movieData.Country,
+            language : movieData.Language,
+            actors : movieData.Actors,     
+         }
+
+         return Movie;
+      },
+
+      parseMovie : function(data){
+         const OMDBHandler = this;
+         const Movie = OMDBHandler.createMovie(JSON.parse(data));
+
+         return Movie;
+      },
+
+      initializeOMDB : function(){
+
+         this.OMDB.apiKey = require('./keys.js').omdb.key;
+         this.OMDB.endpoint = 'https://www.omdbapi.com/';
+         this.OMDB.queryURL = this.OMDB.endpoint + '?apikey=' + this.OMDB.apiKey;
+         this.OMDB.queryURL += "&plot=short";
+         this.OMDB.queryURL += "&tomatoes=true";
 
 
-
-
-
-
-
+      }
 
 	}
 
+OMDBHandler.initializeOMDB();
 
-	return OMBDHandler;
-}
-
+module.exports = OMDBHandler;
 
 
 
